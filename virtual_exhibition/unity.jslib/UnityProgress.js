20fdc61b1cce15d73e67a5e7b2a1885cc5f6b86a
Bonnie <kusasen@gmail.com>
2021-07-21 15:50:05 +0800

    feat: add switch lang function

--- virtual_exhibition/unity.jslib/UnityProgress.js
+++ virtual_exhibition/unity.jslib/UnityProgress.js
@@ -23,32 +23,59 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
 }
 
+
+
+/* iframe 的 parent 就是包它的頁面 */
+// window.addEventListener("load", () => {
+//   parent.postMessage(document.title, "http://127.0.0.1:5501/");
+// });
+
 window.onload = function () {
+  IsPC();
+  
+  // event listening
   let currentLang = localStorage.getItem("lang");
   MultiLanguage(currentLang);
   console.log("currentLang:" + currentLang);
 
   let langSelect = document.querySelector(".select-lang--select");
   langSelect.addEventListener("change", function () {
-    let cur = Number(this.value);
-    let nextLang;
-    switch (cur) {
-      case 1:
-        nextLang = "es";
-        break;
-      case 2:
-        nextLang = "jp";
-        break;
-      default:
-        nextLang = "en";
-    }
+    let cur = this.value;
     // unityInstance.SendMessage("Canvas", "SendToUnity", "es");
-    localStorage.setItem("lang", nextLang);
+    localStorage.setItem("lang", cur);
+    MultiLanguage(cur);
     console.log("select", cur);
     console.log("updated!!");
   });
 };
 
 function MultiLanguage(lang) {
-  unityInstance.SendMessage("Canvas", "SendToUnity", "en");
-}	
+  unityInstance.SendMessage("Canvas", "SendToUnity", lang);
+  unityInstance.SendMessage("Canvas", "loadLanguage");
+}
+
+function IsPC() {
+  var userAgentInfo = navigator.userAgent;
+  var Agents = [
+    "Android",
+    "iPhone",
+    "SymbianOS",
+    "Windows Phone",
+    "iPad",
+    "iPod",
+  ];
+  var flag = true;
+  for (var v = 0; v < Agents.length; v++) {
+    if (userAgentInfo.indexOf(Agents[v]) > 0) {
+      flag = false;
+      break;
+    }
+  }
+
+  unityInstance.SendMessage(
+    "RigidBodyFPSController",
+    "SendToUnity",
+    String(flag)
+  );
+  //unityInstance.SendMessage("BridgeX", "PL_check");
+}
--- virtual_exhibition/unity.jslib/dist/UnityProgress.dev.js
+++ virtual_exhibition/unity.jslib/dist/UnityProgress.dev.js
@@ -25,36 +25,45 @@ function UnityProgress(unityInstance, progress) {
   unityInstance.progress.empty.style.width = 100 * (1 - progress) + "%";
   if (progress == 1) unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
 }
+/* iframe 的 parent 就是包它的頁面 */
+// window.addEventListener("load", () => {
+//   parent.postMessage(document.title, "http://127.0.0.1:5501/");
+// });
+
 
 window.onload = function () {
+  IsPC(); // event listening
+
   var currentLang = localStorage.getItem("lang");
   MultiLanguage(currentLang);
   console.log("currentLang:" + currentLang);
   var langSelect = document.querySelector(".select-lang--select");
   langSelect.addEventListener("change", function () {
-    var cur = Number(this.value);
-    var nextLang;
+    var cur = this.value; // unityInstance.SendMessage("Canvas", "SendToUnity", "es");
 
-    switch (cur) {
-      case 1:
-        nextLang = "es";
-        break;
-
-      case 2:
-        nextLang = "jp";
-        break;
-
-      default:
-        nextLang = "en";
-    } // unityInstance.SendMessage("Canvas", "SendToUnity", "es");
-
-
-    localStorage.setItem("lang", nextLang);
+    localStorage.setItem("lang", cur);
+    MultiLanguage(cur);
     console.log("select", cur);
     console.log("updated!!");
   });
 };
 
 function MultiLanguage(lang) {
-  unityInstance.SendMessage("Canvas", "SendToUnity", "en");
+  unityInstance.SendMessage("Canvas", "SendToUnity", lang);
+  unityInstance.SendMessage("Canvas", "loadLanguage");
+}
+
+function IsPC() {
+  var userAgentInfo = navigator.userAgent;
+  var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+  var flag = true;
+
+  for (var v = 0; v < Agents.length; v++) {
+    if (userAgentInfo.indexOf(Agents[v]) > 0) {
+      flag = false;
+      break;
+    }
+  }
+
+  unityInstance.SendMessage("RigidBodyFPSController", "SendToUnity", String(flag)); //unityInstance.SendMessage("BridgeX", "PL_check");
 }

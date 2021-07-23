05397502ab11f90415843691f32b9217bb8675df
Bonnie <kusasen@gmail.com>
2021-07-23 09:19:05 +0800

    feat: add

--- virtual_exhibition/unity.jslib/dist/UnityProgress.dev.js
+++ virtual_exhibition/unity.jslib/dist/UnityProgress.dev.js
@@ -1,10 +1,11 @@
+"use strict";
+
 function UnityProgress(unityInstance, progress) {
   if (!unityInstance.Module) return;
+
   if (!unityInstance.bg) {
     unityInstance.bg = document.createElement("div");
-    unityInstance.bg.className = `unity-bg unity-bg-${unityInstance.container.getAttribute(
-      "data-idx"
-    )}`;
+    unityInstance.bg.className = "unity-bg unity-bg-".concat(unityInstance.container.getAttribute("data-idx"));
     unityInstance.container.appendChild(unityInstance.bg);
   }
 
@@ -12,19 +13,13 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "unity-progress";
     unityInstance.container.appendChild(unityInstance.progress);
-
     unityInstance.text = document.createElement("div");
-    unityInstance.text.className = `text text-${unityInstance.container.getAttribute(
-      "data-name"
-    )}`;
+    unityInstance.text.className = "text text-".concat(unityInstance.container.getAttribute("data-name"));
     unityInstance.progress.appendChild(unityInstance.text);
-
-    unityInstance.text.innerText =
-      unityInstance.container.getAttribute("data-name");
+    unityInstance.text.innerText = unityInstance.container.getAttribute("data-name");
     unityInstance.progress.bar = document.createElement("div");
     unityInstance.progress.bar.className = "bar";
     unityInstance.progress.appendChild(unityInstance.progress.bar);
-
     unityInstance.progress.full = document.createElement("div");
     unityInstance.progress.full.className = "full";
     unityInstance.progress.bar.appendChild(unityInstance.progress.full);
@@ -41,32 +36,29 @@ function UnityProgress(unityInstance, progress) {
 }
 
 function handelTeachingPopup() {
-  let content = document.querySelector(".unity-teaching");
-  let closeBtn = content.querySelector(".close-popup");
-
+  var content = document.querySelector(".unity-teaching");
+  var closeBtn = content.querySelector(".close-popup");
   content.style.display = "block";
   closeBtn.addEventListener("click", function () {
     content.style.display = "none";
   });
 }
-
 /* iframe 的 parent 就是包它的頁面 */
 // window.addEventListener("load", () => {
 //   parent.postMessage(document.title, "http://127.0.0.1:5501/");
 // });
 
-window.onload = function () {
-  IsPC();
 
-  // event listening
-  let currentLang = localStorage.getItem("lang");
+window.onload = function () {
+  IsPC(); // event listening
+
+  var currentLang = localStorage.getItem("lang");
   MultiLanguage(currentLang);
   console.log("currentLang:" + currentLang);
-
-  let langSelect = document.querySelector(".select-lang--select");
+  var langSelect = document.querySelector(".select-lang--select");
   langSelect.addEventListener("change", function () {
-    let cur = this.value;
-    // unityInstance.SendMessage("Canvas", "SendToUnity", "es");
+    var cur = this.value; // unityInstance.SendMessage("Canvas", "SendToUnity", "es");
+
     localStorage.setItem("lang", cur);
     MultiLanguage(cur);
     console.log("select", cur);
@@ -81,15 +73,9 @@ function MultiLanguage(lang) {
 
 function IsPC() {
   var userAgentInfo = navigator.userAgent;
-  var Agents = [
-    "Android",
-    "iPhone",
-    "SymbianOS",
-    "Windows Phone",
-    "iPad",
-    "iPod",
-  ];
+  var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
   var flag = true;
+
   for (var v = 0; v < Agents.length; v++) {
     if (userAgentInfo.indexOf(Agents[v]) > 0) {
       flag = false;
@@ -97,10 +83,5 @@ function IsPC() {
     }
   }
 
-  unityInstance.SendMessage(
-    "RigidBodyFPSController",
-    "SendToUnity",
-    String(flag)
-  );
-  //unityInstance.SendMessage("BridgeX", "PL_check");
+  unityInstance.SendMessage("RigidBodyFPSController", "SendToUnity", String(flag)); //unityInstance.SendMessage("BridgeX", "PL_check");
 }
